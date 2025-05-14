c7f5eb26d279db59fe236217d900a7b4a745edcc
Joshua Rwanda <joshuarwanda@gmail.com>
2025-05-14 11:15:10 +0300

    added: firestore user storage

--- fanbase-v3/app/providers.tsx
+++ fanbase-v3/app/providers.tsx
@@ -1,9 +1,11 @@
+// app/providers.tsx
 "use client";
 import { config, queryClient } from "@/config";
 import { AlchemyClientState } from "@account-kit/core";
 import { AlchemyAccountProvider } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, Suspense } from "react";
+import { UserProvider } from "@/lib/userContext";
 
 export const Providers = (
   props: PropsWithChildren<{ initialState?: AlchemyClientState }>
@@ -15,7 +17,9 @@ export const Providers = (
         queryClient={queryClient}
         initialState={props.initialState}
       >
-        {props.children}
+        <UserProvider>
+          {props.children}
+        </UserProvider>
       </AlchemyAccountProvider>
     </QueryClientProvider>
   );
--- fanbase-v3/lib/firebase.ts
+++ fanbase-v3/lib/firebase.ts
@@ -1,5 +1,7 @@
 // lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,5 +13,8 @@ const firebaseConfig = {
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
 
+export { db, storage };
 export default app;
